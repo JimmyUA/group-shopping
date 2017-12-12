140b27edb2e0eda9282ef6d164d9b69bcb7863d3
jimmy <sergii.pryhodko@gmail.com>
2017-12-12 21:36:53 +0200

    tried to add grid to orderPage for admin

--- src/main/java/com/sergey/prykhodko/front/pages/admin/admincabinet/order/OrderPageForAdmin.java
+++ src/main/java/com/sergey/prykhodko/front/pages/admin/admincabinet/order/OrderPageForAdmin.java
@@ -2,8 +2,19 @@ package com.sergey.prykhodko.front.pages.admin.admincabinet.order;
 
 import com.sergey.prykhodko.front.pages.basepage.adminbasepage.AdminBasePage;
 import com.sergey.prykhodko.model.order.Order;
+import com.sergey.prykhodko.model.order.suborder.SubOrder;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.DataGridView;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.component.IRequestablePage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderPageForAdmin extends AdminBasePage {
 
@@ -18,6 +29,23 @@ public class OrderPageForAdmin extends AdminBasePage {
         super.onInitialize();
 
         addOrderGeneralInfoLabel();
+        addShopLabel();
+        addGrid();
+    }
+
+    private void addGrid() {
+        ListDataProvider<SubOrder> subOrderListDataProvider = new ListDataProvider<>(order.getSubOrders());
+
+        List<IColumn<SubOrder, String>> columns = new ArrayList<>();
+        columns.add(new PropertyColumn<>(new PropertyModel<>(Model.of("id"), "id"), "id"));
+        DataGridView<SubOrder> dataGridView = new DataGridView<SubOrder>("grid",columns, subOrderListDataProvider){
+
+        };
+        add(dataGridView);
+    }
+
+    private void addShopLabel() {
+        add(new Label("shopLabel", order.getShopName().getName()));
     }
 
     private void addOrderGeneralInfoLabel() {
